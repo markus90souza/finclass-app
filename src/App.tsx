45e27c6e298b94a363c88b57f5893be482cfd809
markus90souza <markus90souza@gmail.com>
2022-04-01 22:07:32 -0300

    Setup theme in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+import { ThemeProvider } from "styled-components/native";
+import themes from "./themes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View>
-      <Text>Its Works</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={themes}>
+      <View>
+        <Text>Its Works</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
